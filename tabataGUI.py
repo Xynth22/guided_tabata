44e50a77994adcde006d6807f79d93e3d613f649
Xynth22 <timjoiner@gmail.com>
2020-01-17 17:28:21 -0600

    Add button to close app when done

--- tabataGUI.py
+++ tabataGUI.py
@@ -1,4 +1,4 @@
-from guizero import App, Text
+from guizero import App, Text, PushButton
 from random import randint
 import time
 import winsound
@@ -6,16 +6,20 @@ import winsound
 exercises = [line.rstrip('\n') for line in open('exercises.txt')]
 
 
-app = App(title="Tapata", width = 2000, height=400, bg="blue")
+app = App(title="Tapata", width = 2000, height=400)
 
-text = Text(app, text="Temp", size=200, align = "bottom")
-counter = Text(app, text="Temp", size=24, align = "top")
+text = Text(app, text="", size=200, align = "bottom")
+counter = Text(app, text="", size=24, align = "top")
 
+def close_app():
+    app.destroy()
+
+closeButton = PushButton(app, text =  "End Workout", align = "right", command = close_app)
 
 frequency = 2500  # Set Frequency To 2500 Hertz
 duration = 50  # Set Duration To 1000 ms == 1 second
 
-for x in range(0,40):
+for x in range(0,20):
     
     #pick an exercise
     i = randint(0,len(exercises)-1);
@@ -38,8 +42,7 @@ for x in range(0,40):
         winsound.Beep(frequency, duration)
         time.sleep(1);    
     winsound.Beep(frequency-500, 150)
- 
-    
+     
 winsound.Beep(frequency+500, 700)
 app.display()
     

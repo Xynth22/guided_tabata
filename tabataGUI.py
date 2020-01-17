ddcfe0b918e8a6c785a8134bab171b51c0b89e76
Xynth22 <timjoiner@gmail.com>
2020-01-17 17:04:30 -0600

    Refactor and add input file for exercise list

--- tabataGUI.py
+++ tabataGUI.py
@@ -3,52 +3,40 @@ from random import randint
 import time
 import winsound
 
-exercises = ["Mountain Climber", 
-"Burpee", 
-"Plank", "Sumo Squat",
-"Wall Sit", "Tricep Raise",
-"Lunge", 
-"SQUAT", 
-"Curtsy Lunge",
-"Calf raise",
-"Standard push-up",
-"Superman",
-"Diamond push-up",
-"Flutter kick",
-"Side plank",
-"Russian twist",
-"Bicycle",
-"Crunch",
-"Full Sit up","Jumping Jacks",
-"High Knee", "Butt Kicks", "Tick Tocks", "Heel Diggs"];
+exercises = [line.rstrip('\n') for line in open('exercises.txt')]
 
-app = App(title="Tapata", width = 2000, height=300)
-text = Text(app, text="Temp", size=200)
-#counter = Text(app, text="Temp", size=24, align = "left")
+
+app = App(title="Tapata", width = 2000, height=400, bg="blue")
+
+text = Text(app, text="Temp", size=200, align = "bottom")
+counter = Text(app, text="Temp", size=24, align = "top")
+
+
 frequency = 2500  # Set Frequency To 2500 Hertz
 duration = 50  # Set Duration To 1000 ms == 1 second
-for x in range(0,10):
+
+for x in range(0,40):
+    
+    #pick an exercise
     i = randint(0,len(exercises)-1);
+    
+    #update text
     text.value = exercises[i]
-    #counter.value = x
-    text = Text(app, text=exercises[i], size=128)
+    counter.value = x        
     app.update()
+
+    #Rest Timer
     time.sleep(7);
-    winsound.Beep(frequency, duration)
-    time.sleep(1);
-    winsound.Beep(frequency, duration)
-    time.sleep(1);
-    winsound.Beep(frequency, duration)
-    time.sleep(1);
+    for y in range(1,3):
+        winsound.Beep(frequency, duration)
+        time.sleep(1);
     winsound.Beep(frequency-500, 150)
     
+    #Excercise timer
     time.sleep(17);
-    winsound.Beep(frequency, duration)
-    time.sleep(1);
-    winsound.Beep(frequency, duration)
-    time.sleep(1);
-    winsound.Beep(frequency, duration)
-    time.sleep(1);
+    for y in range(1,3):
+        winsound.Beep(frequency, duration)
+        time.sleep(1);    
     winsound.Beep(frequency-500, 150)
  
     
